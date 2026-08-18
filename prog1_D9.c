ac817f0f224550f15bd2b29570e1c6904f732272
bhanuja20 <bhanuja.32028@stu.upes.ac.in>
2026-08-18 22:17:30 +0530

    Add files via upload

---
+++ prog1_D9.c
@@ -0,0 +1,67 @@
+/*Q17: Write a program to find the roots of a quadratic equation and categorize them.
+
+Sample Test Cases:
+Input 1:
+1 -3 2
+Output 1:
+Roots are real and different: 2, 1
+
+Input 2:
+1 -2 1
+Output 2:
+Roots are real and same: 1
+
+Input 3:
+1 2 5
+Output 3:
+Roots are complex
+
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float a, b, c;
+	float d, root1, root2;
+	
+	scanf("%f %f %f", &a, &b, &c);
+	
+	d = b * b - 4 * a * c;
+	
+	if(d > 0)
+	{
+		root1 = (-b + sqrt(d))/(2*a);
+		root2 = (-b - sqrt(d))/(2*a);
+		
+		printf("Roots are real and different: %.0f, %.0f", root1, root2);
+	}
+	else if (d == 0)
+	{
+		root1 = -b/(2*a);
+		
+		printf("Roots are real and same: %.0f", root1);
+	}
+	else 
+	{
+		printf("Roots are complex");
+	}
+	
+	return 0;
+}
+
+/*C:\Users\BHANUJA SINGHAL\OneDrive\Desktop>cd c
+
+C:\Users\BHANUJA SINGHAL\OneDrive\Desktop\c>gcc prog1_D9.c -o prog1_D9.out
+
+C:\Users\BHANUJA SINGHAL\OneDrive\Desktop\c>prog1_D9.out
+1 -3 2
+Roots are real and different: 2, 1
+C:\Users\BHANUJA SINGHAL\OneDrive\Desktop\c>prog1_D9.out
+1 -2 1
+Roots are real and same: 1
+C:\Users\BHANUJA SINGHAL\OneDrive\Desktop\c>prog1_D9.out
+1 2 5
+Roots are complex
+*/
